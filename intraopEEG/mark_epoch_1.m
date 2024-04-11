612e5b1fc4eff9d40d6e9559038d8f76014a2943
Mahta Mousavi <mahta.mousavi@gmail.com>
2024-04-11 12:07:37 +0200

    comments/instructions added

--- intraopEEG/mark_epoch_1.m
+++ intraopEEG/mark_epoch_1.m
@@ -1,10 +1,22 @@
 function idx_outlier_1 = mark_epoch_1(EEG, epoch_length)
+%
+% Inputs:
+%   EEG:   EEG data, instance of EEGLAB data structure 
+%   epoch_length: epoch length  
+%
+% Output:
+%   idx_outlier_1: epoch indices marked to be removed later  
+% ------------------------------------------------------------------------
+% This function is part of the intraopEEGtoolbox: 
+% https://github.com/mahtamsv/intraopEEGtoolbox
+%
+% Author: Mahta Mousavi, 2024 
+% ------------------------------------------------------------------------
 
-    % epoch 
-    EEG_epoched = pop_epoch( EEG, {'X'}, [0, epoch_length]);
-    output = double(EEG_epoched.data);
-    
-    % remove epochs that contain no data (one second of flat data)
-    idx_outlier_1 = isflat_mm(output, round(0.5*EEG_epoched.srate));
-    
-end
+% epoch EEG data with the given length 
+EEG_epoched = pop_epoch( EEG, {'X'}, [0, epoch_length]);
+output = double(EEG_epoched.data);
+
+% mark epochs that contain no data (one second of flat data)
+idx_outlier_1 = isflat_mm(output, round(0.5*EEG_epoched.srate));
+
